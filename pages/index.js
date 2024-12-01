219dd8c16be3e3e8420fa3fa8415a7d040b8d972
unknown <farihatusaadah@gmail.com>
2024-12-01 22:19:18 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// /pages/index.js
+export default function Home() {
+    return (
+      <div>
+        <h1>Login & Signup JSON API</h1>
+        <p>Access the JSON file at /userData.json</p>
+      </div>
+    );
+  }
+  
